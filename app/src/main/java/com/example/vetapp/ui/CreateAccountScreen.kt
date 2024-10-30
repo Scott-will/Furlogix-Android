c500a2e17642271d5cbb591d87e908231db65ffa
nicport <nicporter1013@gmail.com>
2024-10-29 21:16:35 -0400

    initial template and basic navigation setup

---
+++ app/src/main/java/com/example/vetapp/ui/CreateAccountScreen.kt
@@ -0,0 +1,46 @@
+package com.example.vetapp.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.vetapp.ui.navigation.Screen
+
+
+@Composable
+fun CreateAccountScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = "", onValueChange = {}, label = { Text("Name") }
+        )
+        TextField(
+            value = "", onValueChange = {}, label = { Text("Surname") }
+        )
+        TextField(
+            value = "", onValueChange = {}, label = { Text("Pet Name") }
+        )
+        TextField(
+            value = "", onValueChange = {}, label = { Text("Email") }
+        )
+        TextField(
+            value = "", onValueChange = {}, label = { Text("Password") }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController.navigate(Screen.Dashboard.route) }) {
+            Text("Create Account")
+        }
+    }
+}
