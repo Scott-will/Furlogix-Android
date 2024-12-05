32ae9c06d1d0935722defb905d3d81cc895e58aa
Scott Williams <scottwill1999@gmail.com>
2024-12-04 22:56:40 -0500

    clean up report sending, add tests for csv

--- app/src/main/java/com/example/vetapp/viewmodels/ReportViewModel.kt
+++ app/src/main/java/com/example/vetapp/viewmodels/ReportViewModel.kt
@@ -116,24 +116,15 @@ class ReportViewModel @Inject constructor(
     }
 
     fun gatherReportData(reportId : Int){
-        /*get all information from database
-        *information is:
-        * report name
-        * templates
-        * data entry
-        *
-        * once all data is retrieved
-        * send to csvBuilder and generate csv file
-        *
-        * send to email handler as file attachment */
         viewModelScope.launch {
             withContext(Dispatchers.IO){
                 val reportName = reportRepository.getReportById(reportId).name
                 val templates = reportTemplateRepository.GetReportById(reportId)
                 val entries = reportEntryRepository.getAllReportEntries(reportId)
-                var fileUri = CsvBuilder().buildCsv(VetApplication.applicationContext(), reportName, entries, templates)
-                val userEmail = userDao.getCurrentUserEmail()
-                val emailWrapper = EmailWrapper(userEmail.value.toString(), userEmail.value.toString(), "${reportName}_${Date()}", fileUri)
+                var fileUri = CsvBuilder().buildAndWriteCsv(VetApplication.applicationContext(), reportName, entries, templates)
+                val user = userDao.getUserById(1)
+                //TODO: Add pet name here
+                val emailWrapper = EmailWrapper(user?.email!!, "Pet Reports", "${reportName}_${Date()}", fileUri)
                 SendEmail(emailWrapper)
             }
 
