e577b5700d7c689836683e3365684725dc373219
nicport <nicporter1013@gmail.com>
2024-12-09 11:57:26 -0500

    adding some styling

--- app/src/main/java/com/example/vetapp/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/vetapp/ui/screens/ProfileScreen.kt
@@ -1,11 +1,13 @@
 package com.example.vetapp.ui.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +19,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -91,6 +95,12 @@ fun ProfileScreen(
         pets.forEach { pet ->
             Button(
                 onClick = { selectedPet = pet },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White,
+                    contentColor = Color(0xFF4A148C)
+                ),
+                //shape = RectangleShape,
+                border = BorderStroke(2.dp, Color(0xFF4A148C)),
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Text(text = pet.name)
