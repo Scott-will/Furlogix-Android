1f2b120cffff0339a516ecf391221191d9a5d552
nicport <nicporter1013@gmail.com>
2024-12-17 20:27:26 -0500

    fixed issue with type in package name

--- app/src/main/java/com/example/vetapp/ui/components/common/ErrorDialog.kt
+++ app/src/main/java/com/example/vetapp/ui/components/common/ErrorDialog.kt
@@ -1,4 +1,4 @@
-package com.example.vetapp.ui.componets.common
+package com.example.vetapp.ui.components.common
 
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
