f28ae40e8e4b193f02b994e293c4446f170678b5
Scott Williams <scottwill1999@gmail.com>
2025-02-23 20:52:15 -0500

    add data button

--- app/src/main/java/com/example/vetapp/ui/screens/ReportEntryHistoryScreen.kt
+++ app/src/main/java/com/example/vetapp/ui/screens/ReportEntryHistoryScreen.kt
@@ -2,10 +2,14 @@ package com.example.vetapp.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,9 +21,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.vetapp.ui.components.reports.ReportHistoryTableItem
+import com.example.vetapp.ui.navigation.Screen
 import com.example.vetapp.viewmodels.ReportViewModel
 
 @Composable
@@ -37,38 +43,46 @@ fun ReportEntryHistoryScreen(navController: NavController, reportId: Int = 0, vi
     val groupedEntries = entries.flatMap { it.value }
         .groupBy { it.timestamp }
 
-    if (entries.isEmpty()) {
-        CircularProgressIndicator(modifier = Modifier.fillMaxSize()) // Show loading indicator
-    } else {
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-            item{
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color.Gray),
-                    horizontalArrangement = Arrangement.Center){
-                    Text(
-                        text = "Timestamp",
-                        modifier = Modifier.weight(1f),
-                        style = TextStyle(fontWeight = FontWeight.Bold, color = Color.Black),
-                        textAlign = TextAlign.Center
-                    )
-                    templates.value.forEach { template ->
+    Column{
+        Button(onClick =
+        {navController.navigate(Screen.ReportEntry.route.replace("{reportId}", reportId.toString()))}){
+            Text("Add Data")
+        }
+        Spacer(modifier = Modifier.fillMaxWidth().padding(10.dp))
+        if (entries.isEmpty()) {
+            CircularProgressIndicator(modifier = Modifier.fillMaxSize()) // Show loading indicator
+        } else {
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                item{
+                    Row(modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.Gray),
+                        horizontalArrangement = Arrangement.Center){
                         Text(
-                            text = template.name,
+                            text = "Timestamp",
                             modifier = Modifier.weight(1f),
                             style = TextStyle(fontWeight = FontWeight.Bold, color = Color.Black),
                             textAlign = TextAlign.Center
                         )
+                        templates.value.forEach { template ->
+                            Text(
+                                text = template.name,
+                                modifier = Modifier.weight(1f),
+                                style = TextStyle(fontWeight = FontWeight.Bold, color = Color.Black),
+                                textAlign = TextAlign.Center
+                            )
+                        }
+
+
                     }
-
-
                 }
-            }
-            groupedEntries.forEach { (timestamp, entryList) ->
-                item{
-                    ReportHistoryTableItem(timestamp, entryList.map{it.value})
+                groupedEntries.forEach { (timestamp, entryList) ->
+                    item{
+                        ReportHistoryTableItem(timestamp, entryList.map{it.value})
+                    }
                 }
             }
         }
     }
+
 }
